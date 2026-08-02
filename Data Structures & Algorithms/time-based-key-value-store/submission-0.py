016e8e9d6d853cdea0109f4a8f559ae87477398c
Tess (Hyunsuh) Lee <tpl52@cornell.edu>
2026-08-02 16:51:42 -0400

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,41 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hashmap = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.hashmap:
+            self.hashmap[key].append((value, timestamp))
+        else: 
+            self.hashmap[key] = [(value, timestamp)]
+
+    def get(self, key: str, timestamp: int) -> str:
+        # strat: find the one that's equal to that timestamp
+        # (instant with hashmap)
+        # if there are none that are equal, use binary search to find the 
+        # largest value associated with the largest timestamp_prev 
+        if key not in self.hashmap: 
+            return ""
+
+        bin_list = self.hashmap[key] # the list of tuples.
+
+        l = 0 
+        m = 0 
+        r = len(bin_list) - 1 
+
+        while l <= r: 
+            m = l + (r - l) // 2
+
+            if bin_list[m][1] == timestamp: 
+                return bin_list[m][0]
+
+            elif bin_list[m][1] < timestamp:
+                l = m + 1 
+            elif bin_list[m][1] > timestamp: 
+                r = m - 1 
+
+        return bin_list[r][0]
+
+
+
+
